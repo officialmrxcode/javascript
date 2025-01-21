ec06a70128cae82feca445ea368d23581b6dfa5f
officialmrxcode <officialmrxcode@gmail.com>
2025-01-21 06:10:51 +0000

    string and number, math

---
+++ basics/02_string.js
@@ -0,0 +1,21 @@
+// strings
+
+let userName="Mr John";
+
+console.log(userName);
+// string values with repeat attr default() not repeat
+//  and we can assign number of repeatation value
+// like: 1,2,3.....etc.
+console.log(userName.repeat());
+// string with concat prop
+console.log(userName.concat(" ",", today we faced earthquake earlier."));
+// string with slice
+console.log(userName.slice(2));
+// another way to create string
+
+let message=new String("Hello Mrs. Mariam");
+// this method store as string value as array 
+console.log(message);
+// if we need to print string value only then we need to use toString prop
+console.log(message.toString());
+
---
+++ basics/03_num_and_math.js
@@ -0,0 +1,23 @@
+const score=400;
+
+const newScore=new Number(12340);
+//console.log(newScore.toFixed());
+
+// 
+
+const hundreds=120000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++ Math +++++++++++++++++++++++++++
+
+// console.log(Math)
+
+console.log(Math.PI);
+console.log(Math.floor(12.9092));
+console.log(Math.sqrt(12));
+console.log(Math.random())
+
+const min=1;
+const max=5;
+
+const first=Math.floor(Math.random()*(max-min)+1)+min;
